ca8ae93369e4473533f492f57c748a6dec19eaee
Lucas Gabriel <lg0111293@gmail.com>
2026-04-05 22:48:56 -0300

    Registro de componentes na camada de serviço

--- src/main/java/com/Projeto_Microservico/Curso/resources/RecursoUsuario.java
+++ src/main/java/com/Projeto_Microservico/Curso/resources/RecursoUsuario.java
@@ -1,20 +1,35 @@
 package com.Projeto_Microservico.Curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Projeto_Microservico.Curso.entities.Usuario;
+import com.Projeto_Microservico.Curso.service.ServicoUsuario;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class RecursoUsuario {
 
-	@GetMapping
-	public ResponseEntity<Usuario> Returno_Usuario(){
-		Usuario user1 = new Usuario(1L,"Lucas","Lucas@gmail.com","1111-1111","admin");
+	@Autowired
+	private ServicoUsuario servico;
 
-		return ResponseEntity.ok().body(user1);
+	@GetMapping
+	public ResponseEntity<List<Usuario>> EncontrarUsuario() {
+		List<Usuario> lista = servico.findAll();
+		return ResponseEntity.ok().body(lista);
+
 	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> EncontrarID(@PathVariable long id) {
+		Usuario obj = servico.encontrarId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
---
+++ src/main/java/com/Projeto_Microservico/Curso/service/ServicoUsuario.java
@@ -0,0 +1,27 @@
+package com.Projeto_Microservico.Curso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Projeto_Microservico.Curso.entities.Usuario;
+import com.Projeto_Microservico.Curso.repositories.RepositorioUsuarios;
+
+@Service
+public class ServicoUsuario {
+
+	@Autowired
+	private RepositorioUsuarios repositorio;
+	
+	public List <Usuario> findAll(){
+		return repositorio.findAll();
+	}
+	
+	public Usuario encontrarId(long id) {
+		Optional<Usuario> obj = repositorio.findById(id);
+		return obj.get();
+	}
+	
+}
